d3849c3ede44b868e4f85606245bdb1c38650d64
YohannesDK <kassaye85@gmail.com>
2021-06-04 15:35:07 +0200

    nice

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                string arg = args[i];
+                Console.WriteLine($"arg: {arg}");
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Enter your name, or (Q, q) to quit: ");
+                string name = Console.ReadLine();
+
+                if (name == "Q" || name == "q")
+                {
+                   break; 
+                }
+                Console.WriteLine($"The number is: {name} \n");
+            }
+
+        }
+    }
+}
